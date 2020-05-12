6d5b1b4343944d4958ef70c5c4684844ef7203ad
mafemont16 <mafemont16@gmail.com>
2020-05-12 08:58:14 +0200

    changed validators in the db

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,12 +3,16 @@ class Event < ApplicationRecord
   has_many :users, through: :participations
   belongs_to :admin, class_name: 'User'
 
-  validates :start_date, :duration, :title, :description, :price, :location, presence: true
-  validates :duration, presence: true
-  validates :title, presence: true, length: {minimum: 5, maximum: 140 }
-  validates :description, presence: true, length: {minimum: 20, maximum: 1000 }
+  validates :start_date, :location, :presence=>true
+  validates :duration,
+            :presence=>true
+  validates :title, :presence=>true, length: {minimum: 5, maximum: 140 }
+  validates :description,
+            :presence=>true,
+            length: {minimum: 20, maximum: 1000 }
   validates :price,
-            presence: true,
-            numericality: {greater_than: 0, less_than_or_equal_to: 1000}, multiple: true
-  validates :location, presence: true
+            :presence=>true
+            numericality: {greater_than: 0, less_than_or_equal_to: 1000}
+  validates :location,
+            :presence=>true
 end
